f08851605875748f445ef395e5a3dca3a27def67
Pawan-bari <pawanbari127@gmail.com>
2025-06-18 14:02:37 +0530

    add registre

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:drawingui/SplashScreen.dart/splashscreen.dart';
 import 'package:drawingui/draw/drawscreen.dart';
+import 'package:drawingui/draw/model/offset.dart';
+import 'package:drawingui/draw/model/stroke.dart';
 import 'package:drawingui/home/homescreen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
 
-void main() {
+
+Hive.registerAdapter(OffsetcustomAdapter());
+Hive.registerAdapter(StrokeAdapter());
+  await Hive.openBox<List<Stroke>>('drawing');
   runApp(const MyApp());
 }
 
