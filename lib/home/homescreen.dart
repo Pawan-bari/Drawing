1dd02bb446612a7bc8c2f58b72776ec4984bf529
Pawan-bari <pawanbari127@gmail.com>
2025-06-18 17:29:48 +0530

    implement Delete feature

--- lib/home/homescreen.dart
+++ lib/home/homescreen.dart
@@ -22,9 +22,29 @@ Future<void>  _initializeHive() async
 {
 
 _drawingbox = Hive.box<List<Stroke>>('drawing');
+setState(() {
+  
+});
 }
 
-
+void _deletedrawing(String name) async {
+  final confirm = await showDialog(
+    context: context, 
+    builder: (context) => AlertDialog(
+      title: Text('Delete Drawing'),
+      content: Text('are u sure want to delete $name'),
+      actions: [
+        TextButton(onPressed: () => Navigator.of(context).pop(false), child: Text('cancel')),
+        TextButton(onPressed: () => Navigator.of(context).pop(true), child: Text('delete'))
+      ],
+    )
+    );
+    if (confirm == true) {
+      _drawingbox.delete(name);
+      setState(() {});
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Drawing $name deleted')));
+    }
+}
 
 
 
@@ -45,7 +65,8 @@ _drawingbox = Hive.box<List<Stroke>>('drawing');
       body: drawingname.isEmpty ? 
       const Center(
         child: Text('No Drawing Saved Yet'),
-      ): GridView.builder(
+      ):
+       GridView.builder(
         padding: const EdgeInsets.all(8),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
@@ -55,20 +76,25 @@ _drawingbox = Hive.box<List<Stroke>>('drawing');
           itemCount: drawingname.length,
        itemBuilder: (context, index){
         final name = drawingname[index];
-        return GestureDetector(
-          onTap: (){
-            Navigator.pushNamed(context, '/draw',
-            arguments: name);
-
-
-          },
-          child: Card(
-            elevation: 4,
-            child: Center(
-              child: Text(name , textAlign: TextAlign.center,
-            style:  TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+        return Stack(
+          children: [
+            GestureDetector(
+              onTap: (){
+                Navigator.pushNamed(context, '/draw',
+                arguments: name);
+            
+            
+              },
+              child: Card(
+                elevation: 4,
+                child: Center(
+                  child: Text(name , textAlign: TextAlign.center,
+                style:  TextStyle(fontSize: 16,fontWeight: FontWeight.bold),),
+                ),
+              ),
             ),
-          ),
+            Positioned(child: IconButton(onPressed: () => _deletedrawing(name), icon: Icon(Icons.delete,color: Colors.red,)))
+          ],
         );
        })
        ,
